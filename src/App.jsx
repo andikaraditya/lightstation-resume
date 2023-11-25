e0dced7f6c3af4f1884e427a81642ff061739e86
Andika <andikaraditya1@gmail.com>
2023-11-25 18:21:03 +0700

    create profile page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import ProfilePage from "./views/ProfilePage"
+
+function App() {
+    return (
+        <>
+            <ProfilePage />
+        </>
+    )
+}
+
+export default App
